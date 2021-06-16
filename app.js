fe02eacde1b11d8b5ef38b04f9f21ccd4d34decf
Mahir Durmaz <durmazmahir99@gmail.com>
2021-06-16 21:29:58 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// console.log('Hello World');
+// console.info("some info");
+// console.warn("some warning");
+// console.error("error!")
+
+// console.log("————————————");
+// var myName = "Mahir";
+// console.log(myName);
+
+// var num = 4;
+
+// console.log(num * num);
+
+// myNema = "Joe";
+
+// console.log(myName);
+
+// var announce = "Welcome to our school"
+// console.log(announce);
+
+// annuonce = "Welcome to Full-stack"
+// console.log(announce);
